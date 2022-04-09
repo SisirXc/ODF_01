be8795edb6c248056250659f0aa9f2bab3d4765a
Sisir <sisirhalder2050@gmail.com>
2022-04-10 02:26:01 +0530

    true commit

---
+++ database/migrations/2022_04_09_184019_create_forums_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->integer('topics')->default(false);
+            $table->integer('user_id');
+            $table->unsignedBigInteger('categoty_id');
+            $table->foreign('categoty_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->integer('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forums');
+    }
+};
